19c256d931f720200f720620522982e124d498cb
harimtim <harimtim@outlook.de>
2024-07-21 14:11:15 +0200

    1.0.4

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import ourpy
-import time
 
 data = {
     "Server": {
@@ -7,16 +6,25 @@ data = {
     }
 }
 
+def job():
+    x = 0
+    while x < 10:
+        print(x)
+        ourpy.delay(1)
+        
+
 ourpy.clear()
 print(ourpy.showconfig())
 print(ourpy.myinfo())
 timer = ourpy.start_timer()
-time.sleep(3.34)
+ourpy.delay(3.34)
 print(str(ourpy.get_timer(timer)).replace(".", ","), "Seconds")
 ourpy.save_json(data, "test.json")
 my_data = ourpy.load_json("test.json")
 my_data["Server"]["Version"] = "1.1"
 my_data["Server"]["Message"] = "Hello from Ourpy!"
 ourpy.save_json(my_data, "test.json")
+ourpy.start_in_thread(job=job)
 print(ourpy.mytime())
 print(ourpy.justtime())
+print(ourpy.get_online_devices())
